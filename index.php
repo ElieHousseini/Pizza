30b02c9e1daa05484a77d4914d276f8369d1a167
Elie Housseini <eliashousseini@gmail.com>
2020-09-12 08:14:29 +0300

    SQL: adding a SELECT command.

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+  // Connect to database
+  $conn = mysqli_connect('localhost','Elie','test1234','ninja_pizza');
+
+  // Check connection
+  if(!$conn){
+    echo 'Connection error: '. mysqli_connect_error();
+  }
+
+  // write query for all pizzas
+  // *: I want all the Cols from the table pizzas
+  $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+  // Make query and get result
+  $result = mysqli_query($conn, $sql);
+
+  // Fetch the resulting rows as an array
+  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // Free result from memory
+  mysqli_free_result($result);
+
+  // Close the connection
+  mysqli_close($conn);
+  
+  print_r($pizzas);
+
+?>
+
+
+
 <!DOCTYPE html>
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">

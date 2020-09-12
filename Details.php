77404b8ee4ce85f7d992f998ffc1af95f9d2e3ad
Elie Housseini <eliashousseini@gmail.com>
2020-09-12 11:35:54 +0300

    adding the delete button with it's functionality

--- Details.php
+++ Details.php
@@ -2,6 +2,22 @@
 
 require('config/db_connect.php');
 
+if(isset($_POST["delete"])){
+
+  $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+  $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+  if(mysqli_query($conn, $sql)){
+    // success
+    header('Location: index.php');
+  } else{
+    // failure
+    echo 'query error: '. mysqli_error($conn);
+  }
+
+}
+
 // Check GET request id param
 if(isset($_GET['id'])){
 
@@ -28,7 +44,7 @@ if(isset($_GET['id'])){
 
 } else{
   // Error
-  echo 'Error fetching the ID';
+  echo "Error finding ID";
 }
 
 ?>
@@ -45,7 +61,15 @@ if(isset($_GET['id'])){
     <p><?php echo date($pizza['created_at']);?></p>
     <h5>Ingredients:</h5>
     <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+    <!-- DETELE FORM -->
+    <form action="Details.php" method = "POST">
+      <input type = "hidden" name = "id_to_delete" value = "<?php echo $pizza['id'] ?>" >
+      <input type="submit" name="delete" value="Delete" class = 'btn brand z-depth-0'>
+    </form>
+
   <?php else: ?>
+    <h5>No Such Pizza Exists</h5>
   <?php endif; ?>
 </div>
 <?php include('Components/Footer.php'); ?>
--- index.php
+++ index.php
@@ -33,21 +33,18 @@
 
 
 <!DOCTYPE html>
-<!-- Compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<link rel="stylesheet" href="Styles.css" type="text/css">
+
 
 <?php include('Components/Header.php'); ?>
 
   <h4 class="center grey-text">Pizzas!</h4>
-
   <div class="container">
 
-    <div class="row">
+<div class="row">
 
       <?php foreach($pizzas as $pizza): ?>
 
-        <div class="col s6 md3">
+   <div class="col s6 md3">
           <div class="card z-depth-0">
             <div class="card-content center">
               <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
@@ -58,13 +55,15 @@
               </ul>
             </div>
             <div class = 'card-action right-align'>
-              <a href="#" class="brand-text">more info</a>
+              <a class="brand-text" href="Details.php?id=<?php echo $pizza['id']?>" >
+              more info
+              </a>
             </div>
           </div>
         </div>
 
       <?php endforeach; ?>
-                  
+
       <?php 
       if(count($pizzas) >= 3): ?>
         <?php  return false?>
@@ -73,9 +72,9 @@
           <p>There are less than 3 pizzas</p>
       <?php endif ?>
     </div>
-
+    <?php include('Components/Footer.php'); ?> 
   </div>
 
-<?php include('Components/Footer.php'); ?>
+
 
 </html>
